333e22fbff6942de01b40c2c2b1669d0b77fc8e9
knacknation <67415117+knacknation@users.noreply.github.com>
2020-07-26 23:06:25 +0600

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,10 +1,34 @@
-<?php $name = $_POST['Name'];
-$email = $_POST['Email'];
-$message = $_POST['Message'];
-$formcontent="From: $name \n Message: $message";
-$recipient = "contact@knacknation.club";
-$subject = "Contact Form";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+<?php
+$field_name = $_POST['Name'];
+$field_email = $_POST['Email'];
+$field_subject = $_POST['Subject'];
+$field_message = $_POST['Message'];
+
+$mail_to = 'contact@knacknation.club';
+$subject = 'Contact Form'.$field_subject;
+
+$body_message = 'From: '.$field_name."\n";
+$body_message .= 'E-mail: '.$field_email."\n";
+$body_message .= 'E-mail: '.$field_subject."\n";
+$body_message .= 'Message: '.$field_message;
+
+$headers = 'From: '.$field_email."\r\n";
+$headers .= 'Reply-To: '.$field_email."\r\n";
+
+$mail_status = mail($mail_to, $subject, $body_message, $headers);
+
+if ($mail_status) { ?>
+	<script language="javascript" type="text/javascript">
+		alert('Thank you for the message. We will contact you shortly if needed.');
+		window.location = 'contact.html';
+	</script>
+<?php
+}
+else { ?>
+	<script language="javascript" type="text/javascript">
+		alert('Message failed. Please, send an email to contact@knacknation.club');
+		window.location = 'contact.html';
+	</script>
+<?php
+}
 ?>
