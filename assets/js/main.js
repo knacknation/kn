50b28a6ec698647e0e5e9d54420eef78d4ebd462
knacknation <67415117+knacknation@users.noreply.github.com>
2020-08-08 10:01:07 +0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -179,7 +179,7 @@ $('.counter-count').each(function () {
         $(this).prop('Counter',0).animate({
             Counter: $(this).text()
         }, {
-            duration: 15000,
+            duration: 17500,
             easing: 'swing',
             step: function (now) {
                 $(this).text(Math.ceil(now));
